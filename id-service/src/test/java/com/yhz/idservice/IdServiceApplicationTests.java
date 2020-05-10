adaa1e390eb75691fe9e6dde8f8b3764df7e7e67
KyleYao <yhz19970815@126.com>
2020-05-10 19:12:02 +0800

    项目初始化

---
+++ id-service/src/test/java/com/yhz/idservice/IdServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yhz.idservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
